7d8ee6a8f9e5b5c3f2171e6cfe20e0b0aacc361a
Mark <markrpg@me.com>
2016-11-24 20:18:52 +0000

    Setup C++ AMP to carry out brute force pair wise Nbody on the GPU through a parallel for.

--- SET10108_Coursework2_NBody/main.cpp
+++ SET10108_Coursework2_NBody/main.cpp
@@ -8,31 +8,61 @@
 //SFML Graphics library for displaying particles
 #include <SFML\Graphics.hpp>
 #include <stdio.h>
-#include <iostream>  
+#include <amp.h>
+#include <amp_math.h>
+#include <iostream>
 #include <fstream>
 #include <math.h>
 
 using namespace std;
 using namespace std::chrono;
+using namespace Concurrency;
+using namespace Concurrency::fast_math;
 
 //Global Variables
-//Number of particles in the scene
 constexpr unsigned int PARTICLECOUNT = 1024;
-//Number of iterations in the scene
-constexpr int ITERATIONS = 10000;
+constexpr int ITERATIONS = 100;
 constexpr int SCREENSIZE = 800;
-constexpr float TIMESTEP = 500.0f;
+constexpr float TIMESTEP = 10.0f;
 constexpr float RESISTANCE = 0.99f;
 
+// structure holding particle data
+struct point
+{
+	float x;
+	float y;
+	float vx;
+	float vy;
+};
+
+//Print Graphics Accelerator info
+void print_amp_info()
+{
+	//Create accelerator
+	accelerator acc;
+
+	wcout << "Device: " << acc.description << endl;
+	wcout << "Dedicated Memory: " << acc.dedicated_memory / 1024 << " MB" << endl;
+	wcout << "Supports Shared Memory: " << acc.supports_cpu_shared_memory << endl;
+	system("pause");
+}
+
 //Function to update display of particles
-void updateDisplay(sf::RenderWindow &window, vector<Particle> *particles)
+void updateDisplay(sf::RenderWindow &window, point *particles)
 {
 	//Clear Window
 	window.clear();
-	for each (Particle var in *particles)
+	for (size_t i = 0; i < PARTICLECOUNT; i++)
 	{
-		window.draw(var);
+		Particle p = Particle(100);
+		p.InitParticle();
+		sf::Vector2f position = { particles[i].x,particles[i].y };
+		sf::Vector2f velocity = { particles[i].vx,particles[i].vy };
+		p.setPosition(position);
+		p.setVelocity(velocity);
+		window.draw(p);
 	}
+
 	//Display Window
 	window.display();
 }
@@ -40,75 +70,94 @@ void updateDisplay(sf::RenderWindow &window, vector<Particle> *particles)
 //Main Function
 int main()
 {
+	print_amp_info();
 	//Create file to store times in MS
 	ofstream data("NBodyBench.csv", ofstream::out);
 	//Initialise Window
 	sf::RenderWindow window(sf::VideoMode(SCREENSIZE, SCREENSIZE), "NBody Simulation - Mark McLaughlin - 40200606");
 	//Particle Vector
-	vector<Particle> particles;
+	//vector<Particle> particles;
 	//Random Number generator
 	default_random_engine e(duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count());
 	//Uniform distribution
 	uniform_real_distribution<float> float_dist(0, SCREENSIZE);
 
+	//Create host memory for C++ Amp
+	point *particles = new point[PARTICLECOUNT];
+
 	//initialise particles
 	for (int i = 0; i < PARTICLECOUNT; i++)
 	{
-		particles.push_back(Particle(100));
-		particles[i].InitParticle();
 		//Set random position
-		particles[i].setPosition(float_dist(e), float_dist(e));
-		//Set random velocity
-		particles[i].setVelocity({0.0f,0.0f});
+		particles[i].x = float_dist(e);
+		particles[i].y = float_dist(e);
+		//Set velocity
+		particles[i].vx = { 0.0f };
+		particles[i].vy = { 0.0f };
 	}
+
+	//Create device buffers - stored on GPU
+	array_view<point, 1> point_buffer(PARTICLECOUNT, particles);
 
 	//Keep count of iterations
 	int count = 0;
 
-	while(window.isOpen() && count < ITERATIONS)
+	while (window.isOpen() && count < ITERATIONS)
 	{
 		//Start Recording time
 		auto start = system_clock::now();
 
-		//Brute-Force Pair Method - NBody
-		for (int i = 0; i < PARTICLECOUNT; i++)
+		auto brute_force = [=](index<1> idx) restrict(amp)
 		{
-			//Local velocity variable used to calculate velocity between all other particles
-			sf::Vector2f velocity = {0.0f, 0.0f};
-			for (int j = 0; j < PARTICLECOUNT; j++)
+
+			//Brute-Force Pair Method - NBod
+
+			//Local velocity variable used to calculate new velocity
+			float vx = 0.0f;
+			float vy = 0.0f;
+
+			for (int ii = 0; ii < PARTICLECOUNT; ii++)
 			{
-				//Dont count same particle
-				if (i == j)
-					continue;
 				//Get distance from two particles
-				sf::Vector2f distance = particles[j].getPosition() - particles[i].getPosition();
-				//Calculating distance squared
-				float dSquared = (distance.x * distance.x + distance.y * distance.y) + 3e4;
-				//If particles are not together
-				if (dSquared > 0.1f)
+				float distx = point_buffer[ii].x - point_buffer[idx].x;
+				float disty = point_buffer[ii].y - point_buffer[idx].y;
+
+				//Calculating DOT product
+				float dot = (distx * distx + disty * disty) + 3e4;
+
+				//If the dot product is valid
+				if (dot > 0.1f)
 				{
 					//Get inverse distance
-					float distSixth = dSquared * dSquared * dSquared;
-					float inverseDist = 1.0f / sqrtf(distSixth);
-					//Add to velocity
-					velocity += distance * inverseDist;
+					float distSixth = dot * dot * dot;
+					float inverseDistance = 1.0f / fast_math::sqrtf(dot);
+
+					//Calculate new velocity
+					vx += distx * inverseDistance;
+					vy += disty * inverseDistance;
 				}
 			}
-			//Calculate new velocity for original particle
-			sf::Vector2f newVelocity = (TIMESTEP * velocity * RESISTANCE) + particles[i].getVelocity();
-			//Set Velocity
-			particles[i].setVelocity(newVelocity);
-			//Check for out of bounds position and fix
-			sf::Vector2f position = particles[i].getPosition();
-			position.x = min(max(position.x, 0.0f), (float)SCREENSIZE);
-			position.y = min(max(position.y, 0.0f), (float)SCREENSIZE);
-			//Update Position
-			particles[i].setPosition(position + newVelocity);
 
-		}
+			//Update Velocity
+			//Calculate new velocity
+			float newVelocityx = (0.01f * vx * 0.01f) + point_buffer[idx].vx;
+			float newVelocityy = (0.01f * vy * 0.01f) + point_buffer[idx].vy;
+
+			//Set Velocity
+			point_buffer[idx].vx = newVelocityx;
+			point_buffer[idx].vy = newVelocityy;
+
+			//update position
+			point_buffer[idx].x += newVelocityx;
+			point_buffer[idx].y += newVelocityy;
+
+		};
+
+		parallel_for_each(point_buffer.extent, brute_force);
+		point_buffer.synchronize();
 
 		//Update display
-		updateDisplay(window,&particles);
+		updateDisplay(window, particles);
 		//Keep iteration count
 		count++;
 		//Output iteration benchmark in ms
@@ -120,6 +169,6 @@ int main()
 
 
 	//Pause 
-    return 0;
+	return 0;
 }
 
